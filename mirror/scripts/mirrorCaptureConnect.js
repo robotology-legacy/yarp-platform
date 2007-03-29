e513c3d00f58200275a10c56125a84c34129ffb3
Claudio Castellini <claudio.castellini@dlr.de>
2007-03-29 16:04:04 +0000

    working version. streams and capture correctly data and images.
    svn path=/trunk/platform/; revision=5092

--- mirror/scripts/mirrorCaptureConnect.js
+++ mirror/scripts/mirrorCaptureConnect.js
@@ -1,6 +1,6 @@
 var WshShell = WScript.CreateObject("WScript.Shell");
 
 WshShell.Run ("yarp connect /mirrorCapture/cmd  /mirrorCollector/cmd", 7, false);
-WshShell.Run ("yarp connect /mirrorCollector/data /mirrorCapture/data ", 7, false);
+WshShell.Run ("yarp connect /net=10/mirrorCollector/data /net=10/mirrorCapture/data ", 7, false);
 WshShell.Run ("yarp connect /net=10/grabber/img0 /net=10/mirrorCapture/img0", 7, false);
 WshShell.Run ("yarp connect /net=10/grabber/img1 /net=10/mirrorCapture/img1", 7, false);
--- mirror/scripts/mirrorCaptureRun.js
+++ mirror/scripts/mirrorCaptureRun.js
@@ -1,6 +1,6 @@
 var WshShell = WScript.CreateObject("WScript.Shell");
 
-WshShell.Run ("C:\\work\\yarp2\\bin\\release\\yarpdev --framerate 25.0 --stamp --device picolo --name /grabber/img0 --unit 0 --width 384 --height 272", 7, false);
-WshShell.Run ("C:\\work\\yarp2\\bin\\release\\yarpdev --framerate 25.0 --stamp --device picolo --name /grabber/img1 --unit 1 --width 384 --height 272", 7, false);
+WshShell.Run ("C:\\work\\yarp2\\bin\\release\\yarpdev --framerate 25.0 --stamp --device picolo --name /grabber/img0 --unit 0 --width 288 --height 204", 7, false);
+WshShell.Run ("C:\\work\\yarp2\\bin\\release\\yarpdev --framerate 25.0 --stamp --device picolo --name /grabber/img1 --unit 1 --width 288 --height 204", 7, false);
 WshShell.Run ("C:\\work\\platform\\mirror\\mirrorCollector\\release\\mirrorCollector", 7, false);
-WshShell.Run ("C:\\work\\platform\\mirror\\mirrorCapture\\release\\mirrorCapture", 1, false);
+//WshShell.Run ("C:\\work\\platform\\mirror\\mirrorCapture\\release\\mirrorCapture", 1, false);
