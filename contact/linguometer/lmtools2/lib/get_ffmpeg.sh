abd23458d8f4a75079345cddb47b9fd16a250286
Michele Tavella <tavella.michele@gmail.com>
2007-05-22 09:01:30 +0000

    updates + new lib folder, with a script to download ffmpeg
    svn path=/trunk/platform/; revision=5453

---
+++ contact/linguometer/lmtools2/lib/get_ffmpeg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget -c http://linguometer.notnot.org/public/ffmpeg-0.4.9-p20070129.tar.bz2
+
+echo "Run"
+echo " tar xvjpf ffmpeg-0.4.9-p20070129.tar.bz2"
+echo " cd ffmpeg/"
+echo " ./configure; make; make install"
