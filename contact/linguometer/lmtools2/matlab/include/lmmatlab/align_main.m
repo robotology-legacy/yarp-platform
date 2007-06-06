c60e6a098e314d2ee0449e9d0636f5633603ca6a
Michele Tavella <tavella.michele@gmail.com>
2007-06-06 07:33:04 +0000

    Updates.
    svn path=/trunk/platform/; revision=5612

--- contact/linguometer/lmtools2/matlab/include/lmmatlab/align_main.m
+++ contact/linguometer/lmtools2/matlab/include/lmmatlab/align_main.m
@@ -3,10 +3,14 @@ clear all;
 opt_plot = 1;
 opt_invert = 0;
 seq = 0;
-num = 0;
+num = 60;
 
 std_rate = 48000;
-filter = ones(round(std_rate*0.2),1);
+filter_fast = ones(round(std_rate*0.01),1);
+filter_slow = ones(round(std_rate*0.1),1);
+if (0)
+	mtPlotFFT(filter_fast, 48e3, 10);
+end
 
 audio_wd  = sprintf('seq_%.4d/wd_%.4d_us.wav', seq, num);
 audio_ag  = sprintf('seq_%.4d/wd_%.4d_ag.wav', seq, num);
@@ -21,11 +25,11 @@ audio_cc  = sprintf('seq_%.4d/wd_%.4d_cc.wav', seq, num);
 
 % Select the right channel (US-Speech)
 wav_wd = owav_wd(:, 2);
-wav_wd = align_zap(wav_wd, std_rate, opt_invert);
+[wav_wd zap0_wd zap1_wd] = align_zap(wav_wd, std_rate, opt_invert);
 % Resample the AG-Speech data (16-->48kHz)
 wav_ag = resample(owav_ag, std_rate, orate_ag);
 rate_ag = std_rate;
-wav_ag = align_zap(wav_ag, std_rate, opt_invert);
+[wav_ag zap0_ag zap1_ag] = align_zap(wav_ag, std_rate, opt_invert);
 % Select the left channel (LG-Speech)
 wav_lg = owav_lg(:, 1);
 wav_lg = resample(wav_lg, std_rate, orate_lg);
@@ -33,17 +37,20 @@ wav_lg = resample(wav_lg, std_rate, orate_lg);
 wav_cc  = owav_cc(:, 1);
 wav_cc = owav_cc(:, 1);
 
-sig_wd   = filter2(filter, abs(wav_wd),  'same');
-sig_ag   = filter2(filter, abs(wav_ag),  'same');
-sig_lg   = filter2(filter, abs(wav_lg),  'same');
-sig_cc   = filter2(filter, abs(wav_cc),  'same');
-sig_cc  = filter2(filter, abs(wav_cc), 'same');
+std_zap0 = max([zap0_wd zap0_ag]);
+std_zap1 = min([zap1_wd zap1_ag]);
+
+sig_wd  = filter2(filter_fast, abs(wav_wd), 'same');
+sig_ag  = filter2(filter_fast, abs(wav_ag), 'same');
+sig_lg  = filter2(filter_fast, abs(wav_lg), 'same');
+sig_cc  = filter2(filter_fast, abs(wav_cc), 'same');
+sig_cc  = filter2(filter_fast, abs(wav_cc), 'same');
 
 printf('Computing offset values:\n');
 offset_ag  = align_lag(sig_wd, sig_ag);
 offset_lg  = align_lag(sig_wd, sig_lg);
 offset_cc = align_lag(sig_wd, sig_cc);
-printf('    WD   %d\n', 0);
+printf('    WD   %d (reference)\n', 0);
 printf('    AG   %d\n', offset_ag);
 printf('    LG   %d\n', offset_lg);
 printf('    CCt  %d\n', offset_cc);
@@ -82,53 +89,56 @@ iag = 2;
 ilg = 3;
 icc = 4;
 
-% ... in order to handle vectors, such
-% as the length vector...
-lengths = zeros(4, 1);
-lengths(iwd) = length(wav_wd);
-lengths(iag) = length(wav_ag);
-lengths(ilg) = length(wav_lg);
-lengths(icc) = length(wav_cc);
-
-% ...and the offset vector
-offsets = zeros(4, 1);
-offsets(iwd) = 0;
-offsets(iag) = offset_ag;
-offsets(ilg) = offset_lg;
-offsets(icc) = offset_cc;
-
-std_lenght = min(lengths);
-std_offset = min(offsets);
 
 % remove NaN values
+wav3_wd = wav_wd;
 wav3_ag = align_crop(wav2_ag, offset_ag);
 wav3_lg = align_crop(wav2_lg, offset_lg);
 wav3_cc = align_crop(wav2_cc, offset_cc);
 
+lengths = zeros(4, 1);
+lengths(iwd) = length(wav3_wd);
+lengths(iag) = length(wav3_ag);
+lengths(ilg) = length(wav3_lg);
+lengths(icc) = length(wav3_cc);
+
+if (std_zap1 <= min(lengths))
+	std_lenght = std_zap1;
+else
+	std_lenght = min(lengths);
+end
+
+wav3_wd = wav3_wd(std_zap0:std_lenght);
+wav3_ag = wav3_ag(std_zap0:std_lenght);
+wav3_lg = wav3_lg(std_zap0:std_lenght);
+wav3_cc = wav3_cc(std_zap0:std_lenght);
 
 
 
 
 
+sig3_wd = filter2(filter_fast, abs(wav3_wd), 'same');
+sig3_ag = filter2(filter_fast, abs(wav3_ag), 'same');
+sig3_lg = filter2(filter_fast, abs(wav3_lg), 'same');
+sig3_cc = filter2(filter_fast, abs(wav3_cc), 'same');
+
+printf('Computing offset values:\n');
+offset3_ag = align_lag(sig3_wd, sig3_ag);
+offset3_lg = align_lag(sig3_wd, sig3_lg);
+offset3_cc = align_lag(sig3_wd, sig3_cc);
+printf('    WD   %d (reference)\n', 0);
+printf('    AG   %d\n', offset3_ag);
+printf('    LG   %d\n', offset3_lg);
+printf('    CCt  %d\n', offset3_cc);
 
 
-
-
-if (1)
-	sig2_ag   = filter2(filter, abs(wav3_ag),  'same');
-	sig2_lg   = filter2(filter, abs(wav3_lg),  'same');
-	sig2_cc  = filter2(filter, abs(wav3_cc), 'same');
-
-	offset_ag  = align_lag(sig_wd, sig2_ag)
-	offset_lg  = align_lag(sig_wd, sig2_lg)
-	offset_cc = align_lag(sig_wd, sig2_cc)
-
+if (opt_plot)
 	mtSimpleFig(2);
 	plot(sig_wd, 'r');
 	hold on;
-	plot(sig2_ag, 'b');
-	plot(sig2_lg, 'k');
-	plot(sig2_cc, 'g');
+	plot(sig3_ag, 'b');
+	plot(sig3_lg, 'k');
+	plot(sig3_cc, 'g');
 	hold off;
 	grid on;
 	axis tight;
@@ -137,3 +147,67 @@ if (1)
 	title('LM Fine Alignment: before');
 	legend('WD', 'AG', 'LG', 'CCt');
 end
+
+if (opt_plot)
+	time = [0:1/std_rate:(length(wav3_wd) - 1)/std_rate];
+	mtSimpleFig(3);
+
+	subplot(4, 2, 1);
+	plot(time, wav3_wd, 'r');
+	grid on;
+	axis tight;
+	ylabel('US-Speech');
+	title('Alignment reference tracks: speech');
+	
+	subplot(4, 2, 3);
+	plot(time, wav3_ag, 'b');
+	grid on;
+	axis tight;
+	ylabel('AG-Speech');
+	
+	subplot(4, 2, 5);
+	plot(time, wav3_lg, 'k');
+	grid on;
+	axis tight;
+	ylabel('LG-Speech');
+	
+	subplot(4, 2, 7);
+	plot(time, wav3_cc, 'g');
+	grid on;
+	axis tight;
+	ylabel('CC-Speech');
+	xlabel('Time [s]');
+		
+	subplot(4, 2, 2);
+	plot(time, filter2(filter_slow, abs(wav3_wd), 'same'), 'r');
+	grid on;
+	axis tight;
+	title('Alignment reference tracks: smooth energy-like');
+	
+	subplot(4, 2, 4);
+	plot(time, filter2(filter_slow, abs(wav3_ag), 'same'), 'b');
+	grid on;
+	axis tight;
+	
+	subplot(4, 2, 6);
+	plot(time, filter2(filter_slow, abs(wav3_lg), 'same'), 'k');
+	grid on;
+	axis tight;
+	
+	subplot(4, 2, 8);
+	plot(time, filter2(filter_slow, abs(wav3_cc), 'same'), 'g');
+	grid on;
+	axis tight;
+	xlabel('Time [s]');
+end
+
+
+audio_wd_out  = sprintf('seq_%.4d/wd_%.4d_us_fine.wav', seq, num);
+audio_ag_out  = sprintf('seq_%.4d/wd_%.4d_ag_fine.wav', seq, num);
+audio_lg_out  = sprintf('seq_%.4d/wd_%.4d_lg_fine.wav', seq, num);
+audio_cc_out  = sprintf('seq_%.4d/wd_%.4d_cc_fine.wav', seq, num);
+
+wavwrite(wav3_wd, std_rate, audio_wd_out);
+wavwrite(wav3_ag, std_rate, audio_ag_out);
+wavwrite(wav3_lg, std_rate, audio_lg_out);
+wavwrite(wav3_cc, std_rate, audio_cc_out);
--- contact/linguometer/lmtools2/matlab/include/lmmatlab/align_zap.m
+++ contact/linguometer/lmtools2/matlab/include/lmmatlab/align_zap.m
@@ -1,12 +1,16 @@
-function wav = align_zap(wav, rate, do_invert)
+function [wav zap0 zap1] = align_zap(wav, rate, do_invert)
 if (do_invert == 0)
     [ignore peak] = max(wav);
-    wav(1:(peak+round(rate*0.1)),:) = 0;
+	zap0 = peak+round(rate*0.1);
+    wav(1:zap0,:) = 0;
     [ignore peak] = min(wav);
-    wav((peak-round(rate*0.1)):length(wav),:) = 0; 
+	zap1 = peak-round(rate*0.1);
+    wav(zap1:length(wav),:) = 0; 
 else
     [ignore peak] = max(wav);
-    wav((peak-round(rate*0.1)):length(wav),:) = 0;
+	zap0 = peak-round(rate*0.1);
+    wav(zap0:length(wav),:) = 0;
     [ignore peak] = min(wav);
-    wav(1:(peak+round(rate*0.1)),:) = 0;
+	zap1 = peak+round(rate*0.1);
+    wav(1:zap1,:) = 0;
 end
