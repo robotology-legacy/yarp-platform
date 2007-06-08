892edaff03903e2e0fe612c66a6e4c8e650b2c5e
Michele Tavella <tavella.michele@gmail.com>
2007-06-08 10:11:11 +0000

    toward pos import
    svn path=/trunk/platform/; revision=5624

--- contact/linguometer/lmtools2/matlab/include/lmmatlab/lmExportAmpProxy.m
+++ contact/linguometer/lmtools2/matlab/include/lmmatlab/lmExportAmpProxy.m
@@ -1,10 +1,10 @@
-lmExportAmp ('temp/ag_sequence_0000.mat', 'temp/ag_sequence_0000.amp');
-lmExportAmp ('temp/ag_sequence_0001.mat', 'temp/ag_sequence_0001.amp');
-lmExportAmp ('temp/ag_sequence_0002.mat', 'temp/ag_sequence_0002.amp');
-lmExportAmp ('temp/ag_sequence_0003.mat', 'temp/ag_sequence_0003.amp');
-lmExportAmp ('temp/ag_sequence_0004.mat', 'temp/ag_sequence_0004.amp');
-lmExportAmp ('temp/ag_sequence_0005.mat', 'temp/ag_sequence_0005.amp');
-lmExportAmp ('temp/ag_sequence_0006.mat', 'temp/ag_sequence_0006.amp');
-lmExportAmp ('temp/ag_sequence_0007.mat', 'temp/ag_sequence_0007.amp');
-lmExportAmp ('temp/ag_sequence_0008.mat', 'temp/ag_sequence_0008.amp');
+lmExportAmp ('temp/ag_sequence_0000_bin.mat', 'temp/ag_sequence_0000.amp');
+lmExportAmp ('temp/ag_sequence_0001_bin.mat', 'temp/ag_sequence_0001.amp');
+lmExportAmp ('temp/ag_sequence_0002_bin.mat', 'temp/ag_sequence_0002.amp');
+lmExportAmp ('temp/ag_sequence_0003_bin.mat', 'temp/ag_sequence_0003.amp');
+lmExportAmp ('temp/ag_sequence_0004_bin.mat', 'temp/ag_sequence_0004.amp');
+lmExportAmp ('temp/ag_sequence_0005_bin.mat', 'temp/ag_sequence_0005.amp');
+lmExportAmp ('temp/ag_sequence_0006_bin.mat', 'temp/ag_sequence_0006.amp');
+lmExportAmp ('temp/ag_sequence_0007_bin.mat', 'temp/ag_sequence_0007.amp');
+lmExportAmp ('temp/ag_sequence_0008_bin.mat', 'temp/ag_sequence_0008.amp');
 quit
--- contact/linguometer/lmtools2/matlab/include/lmmatlab/lmExportPosProxy.m
+++ contact/linguometer/lmtools2/matlab/include/lmmatlab/lmExportPosProxy.m
@@ -1,10 +1,10 @@
-lmExportAmp ('temp/ag_sequence_0000.mat', 'temp/ag_sequence_0000.amp');
-lmExportAmp ('temp/ag_sequence_0001.mat', 'temp/ag_sequence_0001.amp');
-lmExportAmp ('temp/ag_sequence_0002.mat', 'temp/ag_sequence_0002.amp');
-lmExportAmp ('temp/ag_sequence_0003.mat', 'temp/ag_sequence_0003.amp');
-lmExportAmp ('temp/ag_sequence_0004.mat', 'temp/ag_sequence_0004.amp');
-lmExportAmp ('temp/ag_sequence_0005.mat', 'temp/ag_sequence_0005.amp');
-lmExportAmp ('temp/ag_sequence_0006.mat', 'temp/ag_sequence_0006.amp');
-lmExportAmp ('temp/ag_sequence_0007.mat', 'temp/ag_sequence_0007.amp');
-lmExportAmp ('temp/ag_sequence_0008.mat', 'temp/ag_sequence_0008.amp');
+lmExportPos ('temp/ag_sequence_0000_std.pos', 'temp/ag_sequence_0000.pos');
+lmExportPos ('temp/ag_sequence_0001_std.pos', 'temp/ag_sequence_0001.pos');
+lmExportPos ('temp/ag_sequence_0002_std.pos', 'temp/ag_sequence_0002.pos');
+lmExportPos ('temp/ag_sequence_0003_std.pos', 'temp/ag_sequence_0003.pos');
+lmExportPos ('temp/ag_sequence_0004_std.pos', 'temp/ag_sequence_0004.pos');
+lmExportPos ('temp/ag_sequence_0005_std.pos', 'temp/ag_sequence_0005.pos');
+lmExportPos ('temp/ag_sequence_0006_std.pos', 'temp/ag_sequence_0006.pos');
+lmExportPos ('temp/ag_sequence_0007_std.pos', 'temp/ag_sequence_0007.pos');
+lmExportPos ('temp/ag_sequence_0008_std.pos', 'temp/ag_sequence_0008.pos');
 quit
