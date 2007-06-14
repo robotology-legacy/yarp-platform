2874231ecbed9739c24631302a2c52a53aefdd57
Michele Tavella <tavella.michele@gmail.com>
2007-06-14 08:06:19 +0000

    paranoid updates
    svn path=/trunk/platform/; revision=5679

--- contact/linguometer/lmtools2/matlab/include/lmpackages/lmpkgAlignProxy.m
+++ contact/linguometer/lmtools2/matlab/include/lmpackages/lmpkgAlignProxy.m
@@ -1,9 +1,12 @@
 function lmpkgAlignProxy(seq, wd0, wd1)
 
 for wd = wd0:wd1
-	data = lmpkgAlign(seq, wd, 0);
+	data = lmpkgAlign(seq, wd, 1);
 	
 	file_data = sprintf('seq_%.4d/wd_%.4d.mat', seq, wd);
 	printf('[lmpkgAlignProxy] Saving data: %s\n', file_data);
 	save(file_data, '-struct', 'data');
+	clear data;
+    clear file_data;
+    clear global;
 end
