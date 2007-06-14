320b347a3c7bc381505db2940bbd26f280c5538c
Michele Tavella <tavella.michele@gmail.com>
2007-06-14 12:54:38 +0000

    CVS madness
    svn path=/trunk/platform/; revision=5683

--- contact/linguometer/lmtools2/matlab/include/lmpackages/lmpkgAlign.m
+++ contact/linguometer/lmtools2/matlab/include/lmpackages/lmpkgAlign.m
@@ -1,8 +1,24 @@
-%function data = lmpkgAlign(seq, num, opt_plot);
+% Copyright (C) 2007 Michele Tavella <michele@liralab.it>
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-seq = 0;
-num = 28;
-opt_plot = 1;
+function data = lmpkgAlign(seq, num, opt_plot);
+
+%seq = 0;
+%num = 28;
+%opt_plot = 1;
 
 printf('[lmpkgAlign] Running on SEQ %d, word %d\n', seq, num);
 
