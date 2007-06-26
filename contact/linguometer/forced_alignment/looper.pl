30550eb1bd1993545500fee290be322bfbfe52f0
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2007-06-26 10:21:33 +0000

    run over all experiments
    svn path=/trunk/platform/; revision=5725

---
+++ contact/linguometer/forced_alignment/looper.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+# deal with I/O
+
+my $inbase = $ARGV[0];
+my $outbase = $ARGV[1];
+my $code = $ARGV[2];
+if (!defined($code)) {
+    $code = "";
+}
+
+my $exp = "[0-9]+";
+my $seq = "[0-9]+";
+my $word = "[0-9]+";
+
+if ($code=~/^([0-9]+)/) {
+    $exp = sprintf("%04d",$1);
+}
+
+
+if ($code=~/^[0-9]*_([0-9]+)/) {
+    $seq = sprintf("%04d",$1);
+}
+
+if ($code=~/^[0-9]*_[0-9]*_([0-9]+)/) {
+    $word = sprintf("%04d",$1);
+}
+
+my $lst = `cd $inbase; find . -iregex "./exp_$exp/seq_$seq/wd_${word}_us.wav"`;
+#print "cd $inbase; find . -wholename \"./exp_$exp/seq_$seq/wd_${word}_us.wav\"\n";
+chomp($lst);
+
+my @targets = split(/[ \t\n\r]+/,$lst);
+
+print join("\n",@targets), "\n";
+
+foreach my $target (@targets) {
+    if ($target =~ /exp_([0-9]+)\/seq_([0-9]+)\/wd_([0-9]+)/) {
+	my $e = $1;
+	my $s = $2;
+	my $w = $3;
+	print "\n\n\nexp $e seq $s word $w\n";
+	my $OUTDIR = "$outbase/all";
+	my $LABELS = "$inbase/exp_$e/temp/ws_sequence_$s.txt";
+	my $WAVDIR = "$inbase/exp_$e/seq_$s";
+	system "mkdir -p $OUTDIR";
+	system "rm -f /tmp/wd*.gif";
+	system "cat $LABELS | ./process-sequence.pl $WAVDIR $w";
+	system "mv /tmp/wd*.gif $OUTDIR/${e}_${s}_${w}.gif";
+	print "MOVED image to  $OUTDIR/${e}_${s}_${w}.gif\n";
+    }
+}
+
+
+
--- contact/linguometer/forced_alignment/process-sequence.pl
+++ contact/linguometer/forced_alignment/process-sequence.pl
@@ -3,6 +3,10 @@
 use strict;
 
 my $wavdir = $ARGV[0];
+my $filter = -1;
+if ($#ARGV>=1) {
+    $filter = $ARGV[1];
+}
 
 my $ct = 0;
 while (<STDIN>) {
@@ -11,16 +15,18 @@ while (<STDIN>) {
 	my ($idx, $ig1, $txt, $ig2, $ig3, $ig4, $accept) = split(/\;/,$_);
 	my $fname = sprintf("%s/wd_%04d_us.wav",$wavdir,$idx);
 	$txt =~ s/\[.*\]//;
-	print "number $ct, string $txt ($fname)\n";
-	if ($accept) {
-	    system "mkdir -p tmp";
-	    system "cp $fname /tmp/input.wav";
-	    system "echo $fname > tmp/source.txt";
-	    open(FOUT,">/tmp/input.txt");
-	    print FOUT $txt;
-	    close(FOUT);
-	    system "make img";
-	    system sprintf("mv tmp/current.gif /tmp/wd_%04d_us.gif",$idx);
+	if ($filter==-1||$filter==$ct) {
+	    if ($accept) {
+		print "number $ct, string $txt ($fname)\n";
+		system "mkdir -p tmp";
+		system "cp $fname /tmp/input.wav";
+		system "echo $fname > tmp/source.txt";
+		open(FOUT,">/tmp/input.txt");
+		print FOUT $txt;
+		close(FOUT);
+		system "make img";
+		system sprintf("mv tmp/current.gif /tmp/wd_%04d_us.gif",$idx);
+	    }
 	}
 	$ct++;
     }
