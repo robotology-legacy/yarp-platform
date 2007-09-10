db33d4c351ce9dac491bd2a6873f35ae9f5f59e0
Michele Tavella <tavella.michele@gmail.com>
2007-09-10 08:07:14 +0000

    few bugfixes
    svn path=/trunk/platform/; revision=5996

--- contact/linguometer/lmtools2/matlab/include/lmpackages/lmpkgCrop.m
+++ contact/linguometer/lmtools2/matlab/include/lmpackages/lmpkgCrop.m
@@ -16,11 +16,11 @@
 
 function sig2 = lmpkgCrop(sig1, offset)
 if (offset > 0)	
-    %sig2 = sig1;
-    %sig2(1:offset, :) = 0;
+    sig2 = sig1;
+    sig2(1:offset, :) = 0;
 
 	%10 Sept. 2007
-	sig2 = sig1(offset+1:length(sig1));
+	%sig2 = sig1(offset+1:length(sig1));
 	%/10 Sept. 2007
 elseif (offset < 0)
 	%sig2 = sig1(1:length(sig1)+offset-1, :);
