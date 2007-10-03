b4146f7c3f27a86d4c8a3c2cd407eb1a6ff6f34c
Michele Tavella <tavella.michele@gmail.com>
2007-10-03 10:31:34 +0000

    Few updates; experiment 2 added
    svn path=/trunk/platform/; revision=6163

---
+++ contact/linguometer/lmtools2/repo/lm_batchpos/lm_calcpos_0002.bat
@@ -0,0 +1,56 @@
+C:\AG500\npsub.exe path="C:\lm_data\NUM\lm_rawpos" sweepfile=FILE chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\NUM\lm_rawpos" sweepfile=FILE job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0001_0 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0001_0 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0001_1 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0001_1 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0001_2 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0001_2 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0002_0 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0002_0 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0002_1 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0002_1 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0002_2 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0002_2 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0005_0 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0005_0 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0005_1 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0005_1 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0005_2 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0005_2 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0006_0 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0006_0 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0006_1 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0006_1 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0006_2 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0006_2 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0008_0 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0008_0 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0008_1 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0008_1 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0008_2 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0008_2 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0009_0 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0009_0 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0009_1 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0009_1 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0009_2 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0009_2 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0012_0 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0012_0 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0012_1 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0012_1 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0012_2 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0012_2 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0013_0 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0013_0 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0013_1 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0013_1 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0013_2 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0013_2 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0014_0 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0014_0 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0014_1 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0014_1 job=norm
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0014_2 chantype=---------+++ job=refpat
+C:\AG500\npsub.exe path="C:\lm_data\2\lm_rawpos" sweepfile=0014_2 job=norm
